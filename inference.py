b12f0a0f2f5c1a1973a50b8671797fbdd71a92f9
unknown <vivekvardhanmodi@gmial.com>
2026-02-13 09:24:29 +0530

    fix canvas scaling issue

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ from pix2text import Pix2Text
 
 p2t = Pix2Text.from_config()
 while(True):
-    dir = input()
+    dir = input("imagepath: ")
 
     res = p2t.recognize(dir, resized_shape = 608)
     print(res)
